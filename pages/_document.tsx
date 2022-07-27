366f5599886c68e3fdaec7276739177116fffdfa
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2022-07-27 20:22:34 +0530

    Analytics Script component created and configured

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script'
 import { GTM_ID } from '../lib/Gtm'
+import AnalyticsScripts from '@/components/AnalyticsScripts';
 
 const APP_NAME = 'WebinoxMedia Next-TS PWA App'
 
@@ -8,6 +8,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                {/*  Google Tag Manager for Head tag */}
+                <AnalyticsScripts />
+                {/* Google Tag Manager inside Head tag End */}
+
                 <meta name='application-name' content={APP_NAME} />
                 <meta name='apple-mobile-web-app-capable' content='yes' />
                 <meta name='apple-mobile-web-app-status-bar-style' content='default' />
@@ -31,22 +35,8 @@ export default function Document() {
                 <link rel='shortcut icon' href='/icons/favicon.ico' />
             </Head>
             <body>
-                {/*  Google Tag Manager inside Head tag */}
-                <Script id="gtm-script" strategy="beforeInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');`}}></Script>
-                {/* Google Tag Manager inside Head tag End */}
-
-                {/*  Google Tag Manager inside Body tag */}
-                <script>
-                    <iframe
-                        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                        height="0"
-                        width="0"
-                    />
-                </script>
+                {/*  Google Tag Manager code for Body tag */}
+                <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0"></iframe>`}}></noscript>
                 {/* Google Tag Manager inside Body tag End */}
 
                 <Main />
