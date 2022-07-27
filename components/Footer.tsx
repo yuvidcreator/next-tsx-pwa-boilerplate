d1b742b1661af95274f9b2c78a6f0b964f96f94a
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2022-07-28 01:09:00 +0530

    Copyright Seztion added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,25 @@
-import MobileBottomNav from "./MobileBottomNav"
+import { Business } from "@/lib/Web";
+import Link from "next/link";
 
 
 const Footer = () => {
+  const fullYear = new Date().getFullYear();
+
   return (
-    <footer>
-      <h1 className="mb-20 lg:mb-0">Footer</h1>
+    <footer className="mb-20 lg:mb-0">
+      <div className="flex justify-center items-center text-center lg:mb-4">
+        <div>
+          <p>
+            {fullYear} &copy; {Business.site_name} 
+          </p>
+          <p>
+              Designed & Developed by{' '}<br className="md:hidden" />
+              <Link href="https://www.webinoxmedia.com" passHref>
+                <a className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 dark:from-violet-500 dark:to-pink-500">Yuvraj Limbole</a>
+              </Link>
+          </p>
+        </div>
+      </div>
     </footer>
   )
 }
